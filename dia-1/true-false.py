d4d9019a33a7bb69a50c583d6e209ea70c1d7f67
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2021-07-19 11:56:34 -0500

    and not or

--- dia-1/true-false.py
+++ dia-1/true-false.py
@@ -3,3 +3,7 @@ falso=True
 
 print("Verdadero es",verdadero)
 print("Falso es",falso)
+
+print("v y f",verdadero and falso)
+print("v o f",verdadero or falso)
+print("no v y f",not verdadero and falso)
