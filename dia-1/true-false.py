2c76e8efb42d0d9d1a6bd3d58d7060ce47412bc2
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2021-07-19 11:59:37 -0500

    falso verdadero int float

--- dia-1/true-false.py
+++ dia-1/true-false.py
@@ -7,3 +7,10 @@ print("Falso es",falso)
 print("v y f",verdadero and falso)
 print("v o f",verdadero or falso)
 print("no v y f",not verdadero and falso)
+
+verdadero = 10
+falso = 3.1416
+print("Verdadero es",verdadero)
+print("Falso es",falso)
+falso="mi nombre"
+print("Falso es",falso)
