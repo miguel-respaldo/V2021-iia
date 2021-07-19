f0cd5a97e9be823c7a30c202c9c98fbc5d8d6d48
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2021-07-19 11:50:03 -0500

    meses del año

---
+++ dia-1/selecion_multiple.py
@@ -0,0 +1,28 @@
+mes = eval(input("Escribe el número del mes en que naciste"))
+
+if mes == 1:
+    print("Naciste en Enero")
+elif mes == 2:
+    print("Naciste en Febrero")
+elif mes == 3:
+    print("Naciste en Marzo")
+elif mes == 4:
+    print("Naciste en Abril")
+elif mes == 5:
+    print("Naciste en Mayo")
+elif mes == 6:
+    print("Naciste en Junio")
+elif mes == 7:
+    print("Naciste en Julio")
+elif mes == 8:
+    print("Naciste en Agosto")
+elif mes == 9:
+    print("Naciste en Septiembre")
+elif mes == 10:
+    print("Naciste en Octubre")
+elif mes == 11:
+    print("Naciste en Noviembre")
+elif mes == 12:
+    print("Naciste en Diciembre")
+else:
+    print("Ese mes no existe")
