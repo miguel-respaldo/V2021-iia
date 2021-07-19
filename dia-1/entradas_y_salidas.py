63efbe598c56362704dccc1fce17f5546f5de920
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2021-07-19 11:21:10 -0500

    entradas y salidas

---
+++ dia-1/entradas_y_salidas.py
@@ -0,0 +1,9 @@
+# Salida es print()
+# Entrada es input()
+
+print("Este es una calculadora simple")
+print("Suma")
+num1 = eval(input("Escribe el primer número: "))
+num2 = eval(input("Escribe el segundo numero: "))
+resultado = num1 + num2
+print("El resultado es: ", resultado)
