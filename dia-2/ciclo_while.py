05d0b2787ed7e30c1a69d8cf981fade7389eef34
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2021-07-20 08:26:31 -0500

    while 2

--- dia-2/ciclo_while.py
+++ dia-2/ciclo_while.py
@@ -4,3 +4,17 @@ contador = 1
 while contador < 10:
     print(contador)
     contador = contador + 1
+
+print("---------------------")
+
+# mientras contador sea menor o igual a 10
+while contador <= 10:
+    print(contador)
+    contador = contador + 1
+
+print("---------------------")
+
+# mientras contador sea menor o igual a 10
+while contador < 11:
+    print(contador)
+    contador = contador + 1
