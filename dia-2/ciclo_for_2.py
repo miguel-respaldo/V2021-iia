91f46e9f9f855e0dcd3830336574a5321bfa0b54
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2021-07-20 09:06:42 -0500

    otros for

---
+++ dia-2/ciclo_for_2.py
@@ -0,0 +1,22 @@
+for contador in range(5):
+    print(contador)
+
+print("------------------------")
+
+for contador in range(5,10):
+    print(contador)
+
+print("------------------------")
+
+for contador in range(5,0,-1):
+    print(contador)
+
+print("------------------------")
+
+for contador in range(5,-1,-2):
+    print(contador)
+
+print("------------------------")
+
+for contador in range(1,6,2):
+    print(contador)
