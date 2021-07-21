985da5cca80ad8b39b60babf43991e2794b2371d
Miguel Bernal Marin <miguel.bernal.marin@linux.intel.com>
2021-07-21 09:01:49 -0500

    camara 3

---
+++ dia-3/02_camara_espejo.py
@@ -0,0 +1,30 @@
+import cv2 as cv
+
+camara = cv.VideoCapture(0)
+
+# Si la camara NO esta abierta
+if not camara.isOpened():
+    print("No se puede abrir la camara")
+    exit(1)
+
+while True:
+    # Leemos la imagen de la camara
+    ret, imagen = camara.read()
+
+    if not ret:
+        print("No podemos capturar la imagen de la camara")
+        break
+
+    espejo = cv.flip(imagen,1)
+    # 1  espejo
+    # 0  de cabeza
+    # -1 de cabeza espejo
+
+    cv.imshow("Camara", espejo)
+
+    # Si precionamos la tecla Escape
+    if cv.waitKey(1) == 27:
+        break
+
+camara.release()
+cv.destroyAllWindows()
